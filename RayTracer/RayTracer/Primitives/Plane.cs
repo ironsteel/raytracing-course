3c3713a0b37861367e6c2d742160e0a9bc5dfff1
Rangel Ivanov <rangelivanov88@gmail.com>
2013-05-06 17:33:57 +0300

    Added plane primitive

---
+++ RayTracer/RayTracer/Primitives/Plane.cs
@@ -0,0 +1,50 @@
+using System;
+using RayTracer.Core;
+using RayTracer.Math;
+
+namespace RayTracer.Primitives
+{
+	public class Plane : GeomPrimitive
+	{
+
+		public Vector3 point;
+		public Vector3 normal;
+
+		public Plane ()
+		{
+			point = new Vector3 (0, 0, 0);
+			normal = new Vector3 (0, 0, 0);
+		}
+
+
+		#region implemented abstract members of GeomPrimitive
+		
+		public override bool intersect (Ray ray, IntersectionData hitData)
+		{
+			double denom = ray.dir * normal;
+			if (denom < 0.000001)
+				return false;
+
+			double nom = (point - ray.p) * normal;
+			double t = nom / denom;
+
+			hitData.hasIntersection = true;
+			hitData.hitT = t;
+			hitData.hitPos = ray.p + (hitData.hitT * ray.dir);
+			hitData.hitPrimitive = this;
+			hitData.hitNormal = normal;
+
+			return true;
+
+		}
+		
+		public override RayTracer.Math.BoundingBox GetBoundingBox ()
+		{
+			return new BoundingBox(new Vector3(), new Vector3());
+		}
+		
+		#endregion
+
+	}
+}
+
